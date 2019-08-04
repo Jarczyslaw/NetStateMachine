35a52afc7e3cc0656084ac004a14749bb7a9c946
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2019-08-04 21:49:54 +0200

    dev

--- NetStateMachine/Exceptions/TooManyTransitionsException.cs
+++ NetStateMachine/Exceptions/TooManyTransitionsException.cs
@@ -5,7 +5,7 @@ namespace NetStateMachine.Exceptions
     public class TooManyTransitionsException : Exception
     {
         public TooManyTransitionsException(Type sourceState)
-            : base($"There are more valid transitions for state {sourceState}")
+            : base($"There are more than one valid transitions for state {sourceState}")
         {
         }
     }
