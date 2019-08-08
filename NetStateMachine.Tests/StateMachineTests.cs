7fa95af5dbb65b1613eb8bda4da0c0c083ddb617
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2019-08-08 20:47:09 +0200

    dev

---
+++ NetStateMachine.Tests/StateMachineTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NetStateMachine.Tests
+{
+    [TestClass]
+    public class StateMachineTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
