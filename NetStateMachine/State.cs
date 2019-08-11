b1a59a1a5faf7c336d10f2640bab0c57de974d8d
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2019-08-11 14:40:54 +0200

    dev

--- NetStateMachine/State.cs
+++ NetStateMachine/State.cs
@@ -2,16 +2,38 @@
 
 namespace NetStateMachine
 {
+    public delegate void OnStateEnterHandler(OnEnterData data);
+
+    public delegate void OnStateExitHandler(OnExitData data);
+
     public abstract class State
     {
+        private OnStateEnterHandler onStateEnter;
+
+        public event OnStateEnterHandler OnStateEnter
+        {
+            add => onStateEnter += value;
+            remove => onStateEnter -= value;
+        }
+
+        private OnStateExitHandler onStateExit;
+
+        public event OnStateExitHandler OnStateExit
+        {
+            add => onStateExit += value;
+            remove => onStateExit -= value;
+        }
+
         public string Name { get; set; }
 
         public virtual void OnEnter(OnEnterData data)
         {
+            onStateEnter?.Invoke(data);
         }
 
         public virtual void OnExit(OnExitData data)
         {
+            onStateExit?.Invoke(data);
         }
     }
 }
